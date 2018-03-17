c0e6e1d6d283007db0bfa655204f3416a1f7e1c4
Filip Cyprowski <filip.cyprowski@yieldbird.com>
2018-03-17 22:02:55 +0100

    raiseErrors - for refactoring

--- R/PAV_testers.R
+++ R/PAV_testers.R
@@ -3,11 +3,18 @@ require(dplyr)
 require(magrittr)
 require(purrr)
 
-raiseErrorIfFalse = function(lgl, message, message.function = paste) {
+raiseErrorIfFalse = function(lgl, message, message.function = paste, data) {
   lgl %>%
-    when(. ~ .,
+    purrr::when(. ~ data,
        ~stop(message %>%
                message.function))
+}
+
+raiseErrorOrPassForward = function(lgl, message.function, data) {
+  lgl %>%
+    purrr::when(. ~ data,
+                ~stop(message.function()))
+
 }
 
 testIfNamesAreThere = function(data, expected.columns) {
@@ -17,7 +24,7 @@ testIfNamesAreThere = function(data, expected.columns) {
     magrittr::equals(length(expected.columns)) %>%
     raiseErrorIfFalse(
       "These columns are not in the data.frame:",
-      partial(paste, setdiff(expected.columns, names(data)), .first = F)
+      purrr::partial(paste, setdiff(expected.columns, names(data)), .first = F)
     )
 }
 
@@ -28,13 +35,34 @@ testIfThereAreNAs = function(data) {
              na.omit() %>%
              nrow()) %>%
     raiseErrorIfFalse(
-      "NA's in data!"
+      "NA's in data!",
+      data = data
     )
+}
+testWhereAreNAs = function(data) {
+  data %>%
+    map(~which(is.na(.))) %>%
+    keep(is_longer_than, 0) -> result
+
+  result %>%
+    when(length(.) == 0 ~TRUE, ~FALSE) %>%
+    raiseErrorOrPassForward(
+      message.function = partial(
+        map2, result, names(result),
+        ~paste('Column:', .y, 'NAs found on indexes:', toString(.x))
+      ),
+      data = data
+    )
+}
+
+is_longer_than = function(e1, e2) {
+  length(e1) %>% is_greater_than(e2)
 }
 
 testIfTypesAreCorrect = function(data, lTypes) {
   # lTypes - list of functions like "is.numeric" etc.
   data %>%
-    map(~invoke(lTypes, .))
+    purrr::map2(lTypes, ~.y(.x)) %>%
+    map(~raiseErrorIfFalse(., message = 'Data type not as expected'))
 
 }
