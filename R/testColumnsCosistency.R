0aff89a41b2e265a8a5547f8b5885cfd9a1fcbcb
Filip Cyprowski <filip.cyprowski@yieldbird.com>
2018-03-25 21:02:00 +0200

    docs + some style shit

--- R/testColumnsCosistency.R
+++ R/testColumnsCosistency.R
@@ -1,4 +1,17 @@
 #' @importFrom purrr detect_index
+
+#' @title Test data.frame based on some conditions
+#'
+#' @param data data.frame
+#' @param conditions character.vector "filter_ like" conditions, e.g. "Sepal.Length > 0"
+#'
+#' @return the same data.frame or raise error (what conditions were not met)
+#' @export testColumnsCosistency
+#'
+#' @examples
+#' testColumnsCosistency(iris, "Sepal.Length > 0")
+#' iris$Sepal.Length[5:10] = NA
+#' testColumnsCosistency(iris, "is.na(Sepal.Length)")
 testColumnsCosistency = function(data, conditions) {
   # create function for data~result comparison
   other_than_data = partial(other_than_data_, data = data)
@@ -25,15 +38,9 @@ other_than_data_ = function(results, data) {
   nrow(results) != nrow(data) 
 }
 conditions_that_gives_results_ = function(fun, conditions, result) {
-  paste0('Condition ', conditions[detect_index(result, fun)], ' not fulfilled.') %>%
-    paste(collapse = ';\n')
-}
-tell_where_data_fails = function(conditions, result) {
   paste0(
-    'Condition ', 
-    conditions[detect_index(result, other_than_data)],
-    ' not fulfilled.') %>%
-    paste(
-      collapse = ';\n'
-    )
+    'Condition ', conditions[detect_index(result, fun)],
+    ' not fulfilled.'
+  ) %>%
+    paste(collapse = ';\n')
 }
