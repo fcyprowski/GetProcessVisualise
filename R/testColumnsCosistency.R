99275534ffc53a0f750d6c80903e11e5a780740d
Filip Cyprowski <filip.cyprowski@yieldbird.com>
2018-03-25 20:43:24 +0200

    partial evaluation for removing some code smells

--- R/testColumnsCosistency.R
+++ R/testColumnsCosistency.R
@@ -1,4 +1,6 @@
+#' @importFrom purrr detect_index
 testColumnsCosistency = function(data, conditions) {
+  other_than_data = partial(other_than_data_, data = data)
   conditions %>%
     map(~filter_(data, .)) -> result
   result  %>%
@@ -11,6 +13,18 @@ testColumnsCosistency = function(data, conditions) {
     )
     
 }
-other_than_data = function(results) {
-  nrow(results) != nrow(data)
+other_than_data_ = function(results, data) {
+  nrow(results) != nrow(data) 
+}
+conditions_that_data_fails_ = function() {
+  
+}
+tell_where_data_fails = function(conditions, result) {
+  paste0(
+    'Condition ', 
+    conditions[detect_index(result, other_than_data)],
+    ' not fulfilled.') %>%
+    paste(
+      collapse = ';\n'
+    )
 }
